11170d4554ee47576fd4655b89efda0185718cee
Pablo Gonzalez Alvarez <pabloga9@hotmail.com>
2021-09-02 18:45:38 +0200

    Implementado analisis UDP junto con inicio automatico del script http-enum

--- auto_recon.sh
+++ auto_recon.sh
@@ -15,17 +15,15 @@ trap ctrl_c INT
 
 
 function ctrl_c() {
-	echo -e "\n[-] Saliendo...\n"
+	echo -e "\n${redColour}[-] Saliendo...${endColour}\n"
 	exit 1
 }
 
 
 function examples() {
 
-	echo -e 'For nmap tcp scan:\n\tauto_recon -n -t -i 127.0.0.1 -o oN\n'
-	echo -e 'For nmap udp scan:\n\tauto_recon -n -u -i 127.0.0.1 -o oN\n'
-	echo -e 'For nmap version a default scripts scan:\n\tauto_recon -n -v -i 127.0.0.1 -p 22,25,53,80 -o oN\n'
-
+	echo -e '\nFor nmap tcp scan:\n\tauto_recon -n -t -i 127.0.0.1 -o oN\n'
+	echo -e '\nFor nmap udp scan:\n\tauto_recon -n -u -i 127.0.0.1 -o oN\n'
 }
 
 function tcp_scan() {
@@ -35,11 +33,11 @@ function tcp_scan() {
 
 	# Escaneo tcp sobre la direccion indicada y el formato de exportacion indicado
 	nmap -sS --min-rate 5000 -p- --open -vvv -n -Pn $ip_address $export_format allPorts
-	echo "--------------------------------------------------------------------------------"
+	echo -e "${yellowColour}--------------------------------------------------------------------------------${endColour}"
 	getPorts allPorts
-	echo "--------------------------------------------------------------------------------"
+	echo -e "${yellowColour}--------------------------------------------------------------------------------${endColour}"
 
-	echo -e "\n\n--------------Initializing vulnerabilities and version analysis--------------\n\n"
+	echo -e "\n\n${turquoiseColour}[...] Initializing vulnerabilities and version analysis${endColour}\n\n"
 
 	# Automaticamente cuando termine el analisis anterior, realiza un analisis de las vulnerabilidades y version y servicios que corren sobre cada puerto
 	open_ports=$(cat allPorts.tmp | tr -d '\n')
@@ -47,19 +45,50 @@ function tcp_scan() {
 	
 	# Si esta el puerto 80 entre los puertos abiertos realiza un http-enum de nmap
 	echo $open_ports | tr ',' '\n' > openPorts.tmp
-	while FS= read -r line; do
-		if [ '$line' = '80' ]; then
+	while IFS= read -r line; do
+		if [ $line = 80 ]; then
 			# Inicio el script http-enum
 			nmap --script http-enum -p80 $ip_address -oN webScan
 		fi
-	done
+	done < openPorts.tmp
 	
-	
+	# Eliminando archivo temporal de puertos
+	rm openPorts.tmp	
 }
 
-#examples
+function udp_scan() {
 
-while getopts "uti:po:v" arg; do
+	ip_address=$1
+	export_format=$2
+
+	# Escaneo tcp sobre la direccion indicada y el formato de exportacion indicado
+	nmap -sU --min-rate 10000 -p- --open -vvv -n -Pn $ip_address $export_format allPorts
+	echo "--------------------------------------------------------------------------------"
+	getPorts allPorts
+	echo "--------------------------------------------------------------------------------"
+
+	echo -e "\n\n${grayColour}[...] Initializing vulnerabilities and version analysis${endColour}\n\n"
+
+	# Automaticamente cuando termine el analisis anterior, realiza un analisis de las vulnerabilidades y version y servicios que corren sobre cada puerto
+	open_ports=$(cat allPorts.tmp | tr -d '\n')
+	nmap -sV -sC -p$open_ports $ip_address -oN openPorts
+	
+	# Si esta el puerto 80 entre los puertos abiertos realiza un http-enum de nmap
+	echo $open_ports | tr ',' '\n' > openPorts.tmp
+	while IFS= read -r line; do
+		if [ $line = 80 ]; then
+			# Inicio el script http-enum
+			nmap --script http-enum -p80 $ip_address -oN webScan
+		fi
+	done < openPorts.tmp
+	
+	# Eliminando archivo temporal de puertos
+	rm openPorts.tmp	
+}
+
+# Bucle principal para controlar las opciones de escaneo
+
+while getopts "tui:o:h" arg; do
 	case $arg in
 		t) tcp_scan="yes";;
 		u) udp_scan="yes";;
@@ -70,13 +99,13 @@ while getopts "uti:po:v" arg; do
 done
 
 if [ "$tcp_scan" ]; then
-	echo -e "\n${yellowColour}-------------- Initializing tcp scan for $remote_ip as --> nmap -sS --min-rate 5000 -p- --open -n -Pn <ip> <export_format> allPorts --------------${endColour}\n"
+	echo -e "\n${turquoiseColour}[...] Initializing TCP scan for $remote_ip as --> nmap -sS --min-rate 5000 -p- --open -n -Pn <ip> <export_format> allPorts${endColour}\n"
 	tcp_scan $remote_ip $export_format
 fi
 
 
 if [ "$udp_scan" ]; then
-	echo -e "\n${yellowColour}-------------- Initializing udp scan for $remote_ip as --> nmap -sU --min-rate 10000 -p- --open -n -Pn $remote_ip $expor_format allPorts --------------${endColour}\n"
+	echo -e "\n${grayColour}[...] Initializing UDP scan for $remote_ip as --> nmap -sU --min-rate 10000 -p- --open -n -Pn <ip> <export_format> allPorts${endColour}\n"
 	udp_scan $remote_ip $export_format
 fi
 
